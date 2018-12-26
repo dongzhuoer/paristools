781db31da72868e080a20171c743c19dbe72b42b
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2018-12-26 22:47:09 +0800

    develop `read_duplexgroup2()`

--- R/read-dg.R
+++ R/read-dg.R
@@ -60,5 +60,38 @@ read_duplexgroup <- function(path) {
 } 
 
 
-
+#' @title read `.duplexgroup` file
+#' 
+#' @export
+ 
+# path <- 'inst/extdata/Neat1_1.duplexgroup'
+read_duplexgroup2 <- function() {
+    group <- path %>% readr::read_file() %>% stringr::str_split('\n(?=Group )') %>% unlist()
+    
+    group_mat <- stringr::str_split_fixed(group, '---', 2)
+    
+    
+    genome_line <- group_mat[ , 1]
+    reads_lines <- group_mat[ , 2]
+    
+    dplyr::bind_cols(
+        genome_line %>% stringr::str_extract('(?<=position )[^,]+') %>% parse_locs('|'),
+        genome_line %>% stringr::str_extract('\\d+') %>% rep(times = 2) %>% tibble::tibble(id = .)
+        
+    ) %>% tibble::add_column(type = 'genome')
+    
+    
+    reads_lines %>% stringr::str_split_fixed('\t',)[ , 3] %>% parse_locs('|')
+    reads_lines[1:3]
+    genome <- stringr::str_extract(lines[1], '(?<=position )[^,]+')
+    reads  <- stringr::str_split(lines[-1], '\t', simplify = T)[ , 3]
+    
+    dplyr::bind_rows(
+        genome %>% parse_locs('|')   %>% tibble::add_column('type' = 'genome'),
+        reads  %>% parse_locs('<=>') %>% tibble::add_column('type' = 'read')
+    ) %>% tibble::add_column(
+        'id' = stringr::str_extract(lines[1], '\\d+'), 
+        'score' = stringr::str_extract(lines[1], '(?<=score )[\\d\\.]+(?=\\.)')
+    )
+}
 
